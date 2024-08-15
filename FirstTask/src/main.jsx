f8f77ca3a2bcd4960076ac02408c1c04f848b7b6
Yarosikxd <yarikkstenk@gmail.com>
2024-08-15 23:22:20 +0300

    Task completed

---
+++ FirstTask/src/main.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+
+const Heading = (props) =>{
+
+  return <h1 style={{color : props.color}}>{props.text}</h1>
+}
+
+const Description =(props) => {
+  const [userName, setUserName] = useState('');
+
+  useEffect (() => {
+    const name = prompt ("Please enter your name", "Andrew");
+    setUserName(name);
+
+  }, []);
+
+  return <h2 style={{fontStyle: props.textStyle}}>{userName} first React application</h2>
+}
+
+
+createRoot(document.getElementById('root')).render(<>
+
+  <Heading text="Hello, world!" color="crimson"/>
+  <Description textStyle="italic" />
+
+</>)
+
